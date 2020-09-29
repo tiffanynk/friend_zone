4d98831f7729e4a24b67d178f4babb2dd7aa0459
Jon Noriega <Noriega331@gmail.com>
2020-09-29 16:53:12 -0400

    password

--- lib/friend_zone.rb
+++ lib/friend_zone.rb
@@ -8,8 +8,26 @@ class FriendZone
         sleep(1.0)
         puts 'What is your name?'.green
         user = gets.chomp()
+        provide_password()
     end
-    
+   
+    def provide_password
+        system("clear")
+        puts 'Whats your password?'.yellow
+        #sleep pauses code for x amount of seconds
+        sleep(1.0)
+        password_input = gets.chomp()
+        if password_input == "password"
+            puts "Welcome!"
+        else
+            puts "Try again"
+            sleep(1.0)
+            provide_password
+        end
+        
+
+    end
+
     #login or signup
     #see favorites
     #add favorites
