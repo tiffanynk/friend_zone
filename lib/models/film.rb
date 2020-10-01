c7e77bb906dc937df557c0014a1fe20f7b2f1ff9
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-10-01 09:36:02 -0600

    updated user profile and added top 10 list

--- lib/models/film.rb
+++ lib/models/film.rb
@@ -1,4 +1,11 @@
 class Film < ActiveRecord::Base
     has_many :favorite_films
     has_many :users, through: :favorite_films
+
+    #we should probably move this to film.rb
+    # def self.top_ten_films
+    #     #sorts by rt score and returns only 10 films
+    #     self.order(rt_score: :desc).limit(10).pluck(:title)
+    # end
+    # binding.pry
 end
